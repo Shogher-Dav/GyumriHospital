c1ca6de6b0714f8b5e260d0a472b069576f48bb1
Shogher Davtyan <shogher.davtyan95@gmail.com>
2020-06-01 08:54:08 +0400

    add responcivity

--- src/app/home/donations/donations.component.ts
+++ src/app/home/donations/donations.component.ts
@@ -17,37 +17,37 @@ export class DonationsComponent implements OnInit {
                 name: 'Eleonora Saimaon',
                 image: '../../../assets/images/user1.png',
                 amount: 60,
-                date: '20-45-2020'
+                date: '22 Oct 16:04'
             },
             { id: 1,
                 name: 'Anna Smith',
                 image: '../../../assets/images/user2.png',
                 amount: 40,
-                date: '20-45-2020'
+                date: '22 Oct 16:04'
             },
             { id: 1,
                 name: 'Romen Joseph Smith',
                 image: '',
                 amount: 50,
-                date: '20-45-2020'
+                date: '22 Oct 16:04'
             },
             { id: 1,
                 name: 'Eleonora Saimaon',
                 image: '../../../assets/images/user4.png',
                 amount: 70,
-                date: '20-45-2020'
+                date: '22 Oct 16:04'
             },
             { id: 1,
                 name: 'Tontri Qutier',
                 image: '../../../assets/images/user5.png',
                 amount: 80,
-                date: '20-45-2020'
+                date: '22 Oct 16:04'
             },
             { id: 1,
                 name: 'Gilian Silence',
                 image: '../../../assets/images/user6.png',
                 amount: 150,
-                date: '20-45-2020'
+                date: '22 Oct 16:04'
             }
         ];
   }
